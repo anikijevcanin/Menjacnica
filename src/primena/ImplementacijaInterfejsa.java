8691d3601457b25b8c0cdb30b95fc40ac65afa75
anikijevcanin <anisjakijevcanin@gmail.com>
2015-03-31 14:01:27 +0200

    Napravljena klasa ImplementacijaInterfejsa, metode još uvek prazne.

---
+++ src/primena/ImplementacijaInterfejsa.java
@@ -0,0 +1,23 @@
+package primena;
+
+import menjacnica.KursValute;
+
+public class ImplementacijaInterfejsa implements InterfejsMenjacnica {
+
+	public boolean dodajKurs(String naziv, String skraceni, double prodajni,
+			double kupovni, double srednji) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean obrisiKurs(String naziv, int godina, int mesec, int dan) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public KursValute nadjiKurs(String naziv, int godina, int mesec, int dan) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
